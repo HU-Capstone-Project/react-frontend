de1e5e52344f7f98ab2b117558cf2f60b17e76fe
hsntrq <mohdhasantariq@gmail.com>
2023-05-08 02:02:34 +0500

    added about page and fixed color and padding

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import Nodes from "./views/Nodes";
+import About from "./views/About";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Plot } from "./views/Plot";
 
 function App() {
   return (
     <Router>
-      <header className="header" style={{"padding":".5em 1em","display":"flex","justifyContent":"space-between","backgroundColor":"rgba(241,247,253,1)"}}>
+      <header className="header" style={{"padding":"1em 3em","display":"flex","justifyContent":"space-between","backgroundColor":"#061A40"}}>
         <div style={{"fontSize":"1.5em","margin":".6em 0","display":"inline-block"}}>
           <h1 style={{"fontSize":"1.75rem","fontWeight":600}} className="navbar-brand">
-            <a style={{"textDecoration":"none","color":"#445"}}>Road Health Monitoring</a>
+            <a style={{"textDecoration":"none","color":"#FCF7F8"}}>Road Health Monitoring</a>
           </h1>
         </div>
         
       </header>
       <Routes>
         <Route path="/" element={<Nodes />} />
+        <Route path="/about" element={<About />} />
         <Route path="/:id" element={<Plot />} />
       </Routes>
     </Router>

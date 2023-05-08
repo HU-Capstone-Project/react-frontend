857aeda68d5b52616ff9672a17f5bd0c7df9a8cf
hsntrq <mohdhasantariq@gmail.com>
2023-05-08 10:38:20 +0500

    Added Teams page and improved Home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./views/Home";
+import Team from "./views/Team";
 import Nodes from "./views/Nodes";
 import About from "./views/About";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Plot } from "./views/Plot";
 
 function App() {
+  const linkStyle = {
+    textDecoration: "none",
+    color: "#FCF7F8",
+    marginLeft: "1em",
+  };
+
   return (
     <Router>
-      <header className="header" style={{"padding":"1em 3em","display":"flex","justifyContent":"space-between","backgroundColor":"#061A40"}}>
-        <div style={{"fontSize":"1.5em","margin":".6em 0","display":"inline-block"}}>
-          <h1 style={{"fontSize":"1.75rem","fontWeight":600}} className="navbar-brand">
-            <a style={{"textDecoration":"none","color":"#FCF7F8"}}>Road Health Monitoring</a>
+      <header
+        className="header"
+        style={{
+          padding: "1em 3em",
+          display: "flex",
+          justifyContent: "space-between",
+          backgroundColor: "#061A40",
+        }}
+      >
+        <div
+          style={{
+            fontSize: "1.5em",
+            margin: ".6em 0",
+            display: "inline-block",
+          }}
+        >
+          <h1
+            style={{ fontSize: "1.75rem", fontWeight: 600 }}
+            className="navbar-brand"
+          >
+            <Link to="/" style={{ textDecoration: "none", color: "#FCF7F8" }}>
+              Road Health Monitoring
+            </Link>
           </h1>
         </div>
-        
+        <nav>
+          <Link to="/home" style={linkStyle}>
+            Home
+          </Link>
+          <Link to="/about" style={linkStyle}>
+            About
+          </Link>
+          <Link to="/team" style={linkStyle}>
+            Team
+          </Link>
+        </nav>
       </header>
       <Routes>
         <Route path="/" element={<Nodes />} />
         <Route path="/about" element={<About />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/:id" element={<Plot />} />
       </Routes>
     </Router>
---
+++ src/views/Team.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Team() {
+  return (
+    <div>
+      <h2>Our Team</h2>
+      {/* Add the team members and their details */}
+    </div>
+  );
+}
+
+export default Team;
